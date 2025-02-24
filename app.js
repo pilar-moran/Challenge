1286deb89e7de0a69fe59bce0128ea3dfbc097a4
pilar-moran <pmoranf.cl@gmail.com>
2025-02-24 19:53:59 -0300

    Función agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+
+function agregarAmigo() {
+  let input = document.getElementById('amigo');
+  let amigo = input.value;
+  console.log(amigo);
+
+  if (amigo == "") {
+    alert("Por favor, inserte un nombre");
+  }
+  else {
+    amigos.push(amigo);
+    console.log(amigos);
+    console.log(amigos.length);
+
+    input.value = "";
+  }
+};
