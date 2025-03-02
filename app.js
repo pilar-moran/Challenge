fff6b830be79a08db0735dec452b058637c4643c
pilar-moran <pmoranf.cl@gmail.com>
2025-03-01 21:21:26 -0300

    Agregar boton reinicio y su función

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 let amigos = []
 
-
 function agregarAmigo() {
   let inputAmigo = document.getElementById('amigo');
   let amigo = inputAmigo.value;
   console.log(amigo);
-
   if (amigo == "") {
     alert("Por favor, inserte un nombre");
+    return;
   }
   else {
     amigos.push(amigo);
     console.log(amigos);
     console.log(amigos.length);
-
     inputAmigo.value = "";
+    inputAmigo.focus();
     actualizarLista();
   };
-
 };
-
 
 function actualizarLista() {
   let lista =  document.getElementById('listaAmigos');
@@ -31,19 +28,25 @@ function actualizarLista() {
   li.innerText=amigo;
   lista.appendChild(li);
   };
-
 };
 
 function sortearAmigo() {
-  
   if (amigos.length != 0 ) {
   let indice = Math.floor(Math.random() * amigos.length);
   console.log(indice);
   let amigoSecreto = amigos[indice];
   let resultado = document.getElementById('resultado');
-  resultado.innerHTML = amigoSecreto
-
+  resultado.innerHTML = `¡Felicitaciones! el amigo secreto es: ${amigoSecreto}`;
   };
+}; 
 
-};
+function reiniciar() {
+  amigos = [];
+  document.getElementById('listaAmigos').innerHTML = "";
+  document.getElementById('resultado').innerHTML = "";
+  document.getElementById('amigo').value = ""; 
+  document.getElementById('amigo').focus(); 
+}
+
+
 
